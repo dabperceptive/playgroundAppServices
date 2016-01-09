03124b797c18a253c1277ca835e622b4543c4a20
daniel-mba <daniel>
2016-01-09 22:07:51 +0100

    create hello world server

--- src/service/server.js
+++ src/service/server.js
@@ -1,18 +1,20 @@
 'use strict';
 
-var port = process.env.PORT || 8080; // first change
+var port = process.env.PORT || 8080;
 
 var http = require('http');
 var express = require('express');
+
 var bodyParser = require('body-parser');
 var swaggerize = require('swaggerize-express');
-var swaggerUi = require('swaggerize-ui'); // second change
+var swaggerUi = require('swaggerize-ui');
 var path = require('path');
 
 var app = express();
 
 var server = http.createServer(app);
 
+/*
 app.use(bodyParser.json());
 
 app.use(swaggerize({
@@ -23,9 +25,42 @@ app.use(swaggerize({
 
 // change four
 app.use('/docs', swaggerUi({
-  docs: '/swagger'  
+    docs: '/swagger'
 }));
+*/
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
 
 server.listen(port, function () { // fifth change
-    app.setHost(undefined); // sixth and final change
+    //app.setHost(undefined); // sixth and final change
 });
+
+/*
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+*/
